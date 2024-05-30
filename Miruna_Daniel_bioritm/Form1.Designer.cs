daee94a5cdf7e0b7bf46469afb4b1f68b765c625
danielbunea68 <69964215+danielbunea68@users.noreply.github.com>
2024-05-30 19:48:48 +0300

    Add project files.

---
+++ Miruna_Daniel_bioritm/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Miruna_Daniel_bioritm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.button_generare = new System.Windows.Forms.Button();
+			this.dateTimePickerN = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePickerB = new System.Windows.Forms.DateTimePicker();
+			this.label_nastere = new System.Windows.Forms.Label();
+			this.label_bioritm = new System.Windows.Forms.Label();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label_zile = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label_nrzile = new System.Windows.Forms.Label();
+			this.button_zodie = new System.Windows.Forms.Button();
+			this.label_zodie = new System.Windows.Forms.Label();
+			this.pictureBox_zodie = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_zodie)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button_generare
+			// 
+			this.button_generare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			this.button_generare.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_generare.Location = new System.Drawing.Point(535, 54);
+			this.button_generare.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.button_generare.Name = "button_generare";
+			this.button_generare.Size = new System.Drawing.Size(152, 47);
+			this.button_generare.TabIndex = 0;
+			this.button_generare.Text = "Generare Bioritm";
+			this.button_generare.UseVisualStyleBackColor = false;
+			this.button_generare.Click += new System.EventHandler(this.button_generare_Click);
+			// 
+			// dateTimePickerN
+			// 
+			this.dateTimePickerN.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dateTimePickerN.Location = new System.Drawing.Point(16, 63);
+			this.dateTimePickerN.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.dateTimePickerN.Name = "dateTimePickerN";
+			this.dateTimePickerN.Size = new System.Drawing.Size(151, 23);
+			this.dateTimePickerN.TabIndex = 1;
+			this.dateTimePickerN.ValueChanged += new System.EventHandler(this.dateTimePickerN_ValueChanged);
+			// 
+			// dateTimePickerB
+			// 
+			this.dateTimePickerB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dateTimePickerB.Location = new System.Drawing.Point(232, 63);
+			this.dateTimePickerB.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.dateTimePickerB.Name = "dateTimePickerB";
+			this.dateTimePickerB.Size = new System.Drawing.Size(192, 23);
+			this.dateTimePickerB.TabIndex = 2;
+			this.dateTimePickerB.ValueChanged += new System.EventHandler(this.dateTimePickerB_ValueChanged);
+			// 
+			// label_nastere
+			// 
+			this.label_nastere.AutoSize = true;
+			this.label_nastere.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_nastere.Location = new System.Drawing.Point(16, 28);
+			this.label_nastere.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_nastere.Name = "label_nastere";
+			this.label_nastere.Size = new System.Drawing.Size(120, 25);
+			this.label_nastere.TabIndex = 3;
+			this.label_nastere.Text = "Data nasterii";
+			// 
+			// label_bioritm
+			// 
+			this.label_bioritm.AutoSize = true;
+			this.label_bioritm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_bioritm.Location = new System.Drawing.Point(228, 28);
+			this.label_bioritm.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_bioritm.Name = "label_bioritm";
+			this.label_bioritm.Size = new System.Drawing.Size(115, 25);
+			this.label_bioritm.TabIndex = 4;
+			this.label_bioritm.Text = "Data bioritm";
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(492, 501);
+			this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.trackBar1.Maximum = 30;
+			this.trackBar1.Minimum = 1;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(557, 56);
+			this.trackBar1.TabIndex = 5;
+			this.trackBar1.Value = 1;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(4, 108);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1045, 389);
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(493, 546);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(313, 17);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Numarul de zile pentru reprezentarea bioritmului";
+			// 
+			// label_zile
+			// 
+			this.label_zile.AutoSize = true;
+			this.label_zile.Location = new System.Drawing.Point(687, 546);
+			this.label_zile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_zile.Name = "label_zile";
+			this.label_zile.Size = new System.Drawing.Size(0, 17);
+			this.label_zile.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.SystemColors.Control;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(28, 520);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(54, 29);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "fizic";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.SystemColors.Control;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.Blue;
+			this.label3.Location = new System.Drawing.Point(111, 520);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(119, 29);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "emotional";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.SystemColors.Control;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.Green;
+			this.label4.Location = new System.Drawing.Point(253, 520);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(122, 29);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "intelectual";
+			// 
+			// label_nrzile
+			// 
+			this.label_nrzile.AutoSize = true;
+			this.label_nrzile.BackColor = System.Drawing.Color.Fuchsia;
+			this.label_nrzile.Location = new System.Drawing.Point(817, 549);
+			this.label_nrzile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_nrzile.Name = "label_nrzile";
+			this.label_nrzile.Size = new System.Drawing.Size(0, 17);
+			this.label_nrzile.TabIndex = 12;
+			// 
+			// button_zodie
+			// 
+			this.button_zodie.BackColor = System.Drawing.Color.Red;
+			this.button_zodie.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_zodie.Location = new System.Drawing.Point(1070, 46);
+			this.button_zodie.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.button_zodie.Name = "button_zodie";
+			this.button_zodie.Size = new System.Drawing.Size(152, 55);
+			this.button_zodie.TabIndex = 13;
+			this.button_zodie.Text = "Afla zodia";
+			this.button_zodie.UseVisualStyleBackColor = false;
+			this.button_zodie.Click += new System.EventHandler(this.button_zodie_Click);
+			// 
+			// label_zodie
+			// 
+			this.label_zodie.AutoSize = true;
+			this.label_zodie.BackColor = System.Drawing.Color.Lime;
+			this.label_zodie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_zodie.Location = new System.Drawing.Point(1081, 168);
+			this.label_zodie.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_zodie.Name = "label_zodie";
+			this.label_zodie.Size = new System.Drawing.Size(61, 25);
+			this.label_zodie.TabIndex = 14;
+			this.label_zodie.Text = "Zodia";
+			// 
+			// pictureBox_zodie
+			// 
+			this.pictureBox_zodie.Location = new System.Drawing.Point(1057, 259);
+			this.pictureBox_zodie.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.pictureBox_zodie.Name = "pictureBox_zodie";
+			this.pictureBox_zodie.Size = new System.Drawing.Size(311, 238);
+			this.pictureBox_zodie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox_zodie.TabIndex = 15;
+			this.pictureBox_zodie.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(1370, 749);
+			this.Controls.Add(this.pictureBox_zodie);
+			this.Controls.Add(this.label_zodie);
+			this.Controls.Add(this.button_zodie);
+			this.Controls.Add(this.label_nrzile);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label_zile);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.label_bioritm);
+			this.Controls.Add(this.label_nastere);
+			this.Controls.Add(this.dateTimePickerB);
+			this.Controls.Add(this.dateTimePickerN);
+			this.Controls.Add(this.button_generare);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_zodie)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_generare;
+        private System.Windows.Forms.DateTimePicker dateTimePickerN;
+        private System.Windows.Forms.DateTimePicker dateTimePickerB;
+        private System.Windows.Forms.Label label_nastere;
+        private System.Windows.Forms.Label label_bioritm;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_zile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_nrzile;
+        private System.Windows.Forms.Button button_zodie;
+        private System.Windows.Forms.Label label_zodie;
+        private System.Windows.Forms.PictureBox pictureBox_zodie;
+    }
+}
+
---
+++ Miruna_Daniel_bioritm/Form1.cs
@@ -0,0 +1,211 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miruna_Daniel_bioritm
+{
+    public partial class Form1 : Form
+    {
+        Bitmap b;
+        int lx, ly;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lx = pictureBox1.Width; ly = pictureBox1.Height;
+            b = new Bitmap(lx, ly);
+            pictureBox1.Image = b;
+            dateTimePickerB.Value = DateTime.Now;
+        }
+
+        private void dateTimePickerN_ValueChanged(object sender, EventArgs e)
+        {
+            button_generare_Click(sender, e);
+        }
+
+        private void dateTimePickerB_ValueChanged(object sender, EventArgs e)
+        {
+            button_generare_Click(sender, e);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label_nrzile.Text = trackBar1.Value.ToString();
+            button_generare_Click(sender, e);
+        }
+
+
+
+        private void button_generare_Click(object sender, EventArgs e)
+        {
+            if (b == null) return;
+            Graphics g = Graphics.FromImage(b);/// incepem sa desenam sistemul de axe cu variabila g din clasa Graphics
+            g.Clear(Color.LightYellow);/// dam culoare fontului 
+            g.DrawLine(new Pen(Color.Black, 2), 0, ly / 2, lx, ly / 2);/// desenam axa ox
+            g.DrawLine(new Pen(Color.Black, 2), lx / 2, 0, lx / 2, ly);/// desenam axa oy
+            string ziua;
+            int nr_zile = trackBar1.Value;/// luam numarul de zile din track bar - calc bioritm pe cate zile vrem
+            double dx = lx / (2.0 * nr_zile);/// diferenta dintre 2 linii verticale , intr-un numar real
+            // Desenare Cadrane
+            for (int i = 0; i <= 2 * nr_zile; i++)
+            {
+                ziua = dateTimePickerB.Value.Date.AddDays(i - nr_zile).ToShortDateString();///incepem de la minus mergem n poziti stanaga si n poziti dreapta de aceea si forul pana la 2*n
+                g.DrawLine(new Pen(Color.Black, 1), (int)(i * dx), 0, (int)(i * dx), ly); /// desenam linia 
+                g.DrawString(ziua, new Font("Arial", 10),
+                new SolidBrush(Color.Blue), (int)(i * dx), ly - 80, new StringFormat(StringFormatFlags.DirectionVertical));/// dam font datei , ii dam coordonatele si cum este asezata
+            }
+            // Desenare 
+            DateTime zi1 = dateTimePickerB.Value.Date.AddDays(-nr_zile);/// ziua din partea stanga 
+            DateTime zi2 = dateTimePickerB.Value.Date.AddDays(nr_zile);/// ziua din partea dreapta
+            //
+            int nrz1 = (zi1 - dateTimePickerN.Value.Date).Days;/// diferenta de zile dintre data anterioara si data nasterii
+            int nrz2 = (zi2 - dateTimePickerN.Value.Date).Days;/// diferenta de zile dintre data urmatoare  si data nasterii
+                                                               /// tre sa repr functiile sin(2*pi*t/23),(../28),....(/33)
+            int x;
+            for (x = 0; x <= lx; x++)
+            {
+                double xsin1 = nrz1 + (double)x / lx * (nrz2 - nrz1); /// lx suparafata de desenare in pixeli
+                double xsin2 = nrz1 + (double)(x + 1) / lx * (nrz2 - nrz1);/// varialbila xsin1 sau xsin2 creste intre nrz1 si nrz2 ;daca x =0 atunci xsin este nrz1 si daca x= lx atunci xsin este nrz2
+                /// deci practic calculam nr de zile la fiecare zi din intervalul de zile pe care ni-l da trackbarul
+                g.DrawLine(new Pen(Color.Red, 2), 
+                    x, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / 23) * (ly / 2 - 10)),
+                    x + 1,  ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / 23) * (ly / 2 - 10)) );
+                g.DrawLine(new Pen(Color.Blue, 2), x, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / 28) * (ly / 2 - 10)),
+                    x + 1, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / 28) * (ly / 2 - 10)));
+                g.DrawLine(new Pen(Color.Green, 2), x, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / 33) * (ly / 2 - 10)),
+                    x + 1, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / 33) * (ly / 2 - 10)));
+            }
+            
+            pictureBox1.Refresh();
+
+        }
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button_zodie_Click(object sender, EventArgs e)
+        {
+            /// Varsator
+            if (dateTimePickerN.Value.Month == 1 && dateTimePickerN.Value.Day >= 20 || dateTimePickerN.Value.Month == 2 && dateTimePickerN.Value.Day <= 18)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\varasator.jpg");
+                label_zodie.Text = "Varasator";
+            }
+            ///Pesti
+
+            if (dateTimePickerN.Value.Month == 2 && dateTimePickerN.Value.Day >= 19 || dateTimePickerN.Value.Month == 3 && dateTimePickerN.Value.Day <= 20)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\pesti.jpg");
+                label_zodie.Text = "Pesti";
+            }
+             /// Berbec
+
+            if (dateTimePickerN.Value.Month == 3 && dateTimePickerN.Value.Day >= 21 || dateTimePickerN.Value.Month == 4 && dateTimePickerN.Value.Day <= 20)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\berbec.jpg");
+                label_zodie.Text = "Berbec";
+            }
+            /// Taur
+
+            if (dateTimePickerN.Value.Month == 4 && dateTimePickerN.Value.Day >= 21 || dateTimePickerN.Value.Month == 5 && dateTimePickerN.Value.Day <= 20)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\taur.jpg");
+                label_zodie.Text = "Taur";
+            }
+
+            ///Gemeni
+
+            if (dateTimePickerN.Value.Month == 5 && dateTimePickerN.Value.Day >= 21|| dateTimePickerN.Value.Month == 6 && dateTimePickerN.Value.Day <= 21)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\gemeni.jpg");
+                label_zodie.Text = "Gemeni";
+            }
+
+            /// Rac
+            
+            if (dateTimePickerN.Value.Month == 6 && dateTimePickerN.Value.Day >= 22 || dateTimePickerN.Value.Month == 7 && dateTimePickerN.Value.Day <= 22)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\rac.jpg");
+                label_zodie.Text = "Rac";
+            }
+            /// Leu
+
+            if (dateTimePickerN.Value.Month == 7 && dateTimePickerN.Value.Day >= 23 || dateTimePickerN.Value.Month == 8 && dateTimePickerN.Value.Day <= 22)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\leu.jpg");
+                label_zodie.Text = "Leu";
+            }
+            /// Fecioara
+
+            if (dateTimePickerN.Value.Month == 8 && dateTimePickerN.Value.Day >= 23 || dateTimePickerN.Value.Month == 9 && dateTimePickerN.Value.Day <= 22)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\fecioara.jpg");
+                label_zodie.Text = "Fecioara";
+            }
+            /// Balanta
+
+            if (dateTimePickerN.Value.Month == 9 && dateTimePickerN.Value.Day >= 23 || dateTimePickerN.Value.Month == 10 && dateTimePickerN.Value.Day <= 22)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\balanta.jpg");
+                label_zodie.Text = "Balanta";
+            }
+            /// Scorpion
+
+            if (dateTimePickerN.Value.Month == 10 && dateTimePickerN.Value.Day >= 23|| dateTimePickerN.Value.Month == 11 && dateTimePickerN.Value.Day <= 21)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\scorpion.jpg");
+                label_zodie.Text = "Scorpion";
+            }
+
+            /// Sagetator
+
+            if (dateTimePickerN.Value.Month == 11 && dateTimePickerN.Value.Day >= 22 || dateTimePickerN.Value.Month == 12 && dateTimePickerN.Value.Day <= 20)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\sagetator.jpg");
+                label_zodie.Text = "Sagetator";
+            }
+
+            /// Capricorn
+
+            if (dateTimePickerN.Value.Month == 12 && dateTimePickerN.Value.Day >= 21|| dateTimePickerN.Value.Month == 1 && dateTimePickerN.Value.Day <= 19)
+            {
+                pictureBox_zodie.Image = Image.FromFile(@"imagini\capricorn.jpg");
+                label_zodie.Text = "Capricorn";
+            }
+        }
+
+       /* private void DrawSin(Graphics g, int t, int nrz1, int nrz2, Color c)
+        {
+            int x;
+            for (x = 0; x <= lx; x++)
+            {
+                double xsin1 = nrz1 + (double)x / lx * (nrz2 - nrz1);
+                double xsin2 = nrz1 + (double)(x + 1) / lx * (nrz2 - nrz1);
+                g.DrawLine(
+                    new Pen(c, 2),
+                    x,
+                    ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / t) * (ly / 2 - 10)),
+                    x + 1,
+                    ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / t) * (ly / 2 - 10))
+                );
+                //g.DrawLine(new Pen(Color.Blue, 2), x, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / 28) * (ly / 2 - 10)),
+                   // x + 1, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / 28) * (ly / 2 - 10)));
+               // g.DrawLine(new Pen(Color.Green, 2), x, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin1 / 33) * (ly / 2 - 10)),
+                   // x + 1, ly / 2 + (int)(Math.Sin(2 * Math.PI * xsin2 / 33) * (ly / 2 - 10)));
+            }
+        }*/
+
+        
+    }
+}
